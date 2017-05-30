c2117c75d0aaf17e1941edb4a90c61ecb394fb03
Eddie Fisher <eddi.fisher@googlemail.com>
2017-05-30 15:38:17 +0200

    blogpost created May 30, 2017 by gonew

---
+++ src/main.go
@@ -0,0 +1,13 @@
+// Copyright 2017, Eddie Fisher. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// main.go [created: Tue, 30 May 2017]
+
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello, Eddie Fisher!")
+}
